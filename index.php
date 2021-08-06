eb8bcd0e9e89b2188bdb5c563db8ed9d5c69586c
arieginulur <arieginulur@gmail.com>
2021-08-06 09:18:50 +0700

    commit on main

---
+++ index.php
@@ -0,0 +1,75 @@
+<!--  
+
+    Link Github     :
+    NIM             : 10918020
+    Nama            : Ari Dwi Ginulur
+    Kelas           : MI-1
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nilai Mahasiswa</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+        body{
+            margin: 0;
+            font-size: 1rem;
+            font-weight: 400;
+            background-color: #ffffff;
+        }
+        .content{
+            padding-right: 7.5px;
+            padding-left: 7.5px;
+            margin-right: auto;
+            margin-left: auto;
+            margin-top : 10px;
+        }
+    </style>
+</head>
+<body >
+<div class="wrapper">
+    <nav class="navbar navbar-dark bg-primary">
+        <span class="navbar-brand mb-0 h1">Aplikasi Nilai Mahasiswa</span>
+    </nav>
+    <section class="container">
+        <div class="content">
+            <h4>Data Nilai Mahasiswa</h4>
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr align="center">
+                                <th>NIM</th>
+                                <th>NAMA</th>
+                                <th>TUGAS</th>
+                                <th>UTS</th>
+                                <th>UAS</th>
+                                <th>NA</th>
+                                <th>MATKUL</th>
+                                <th>DOSEN</th>
+                                <th>#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>       
+    </section>
+
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
